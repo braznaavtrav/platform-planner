84e1e60f5d2b0ad1af2ba18c293f5cfb46c713dc
Travis McKinney <travis.r.mckinney@gmail.com>
2014-01-19 00:31:30 -0500

    gonna go with box2djs and easel

--- js/main.js
+++ js/main.js
@@ -17,6 +17,9 @@ require.config({
       // }
       box2dweb: {
         exports: 'Box2D'
+      },
+      easeljs: {
+        exports: 'createjs'
       }
     },
     baseUrl: 'js/',
@@ -38,7 +41,7 @@ require.config({
 });
 
 require(['jquery', 'box2dweb', 'easeljs'], 
-  function ($, Box2D) {
+  function ($, Box2D, createjs) {
     'use strict';
     
     var box2d = {
@@ -56,17 +59,28 @@ require(['jquery', 'box2dweb', 'easeljs'],
 
     var SCALE = 30,
         stage,
-        world;
+        world,
+        debug;
 
     stage = new createjs.Stage(document.getElementById('display'));
+    debug = document.getElementById('debug');
 
     setUpPhysics();
 
-    createjs.Ticker.addListener(this);
+    $('#display').on( 'click', function (e) {
+      var b = new Ball();
+      console.log(b.view.onTick);
+      stage.addChild(b.view);
+    });
+
+    createjs.Ticker.addEventListener('tick', function() {
+      tick();
+    });
+
     createjs.Ticker.setFPS(60);
     createjs.Ticker.useRAF = true;
 
-    function setupPhysics() {
+    function setUpPhysics() {
       // create world
       world = new box2d.b2World(new box2d.b2Vec2(0, 50), true);
 
@@ -76,22 +90,50 @@ require(['jquery', 'box2dweb', 'easeljs'],
       fixDef.friction = 0.5;
       var bodyDef = new box2d.b2BodyDef();
       bodyDef.type = box2d.b2Body.b2_staticBody;
-      bodyDef.position.x = 400 / SCALE;
-      bodyDef.position.y = 600 / SCALE;
+      bodyDef.position.x = 1024 / 2 / SCALE;
+      bodyDef.position.y = (768 / SCALE) - (5 / SCALE);
       fixDef.shape = new box2d.b2PolygonShape();
-      fixDef.shape.SetAsBox(400 / SCALE, 10 / SCALE);
+      fixDef.shape.SetAsBox(1024 / SCALE, 10 / SCALE);
       world
         .CreateBody(bodyDef)
         .CreateFixture(fixDef);
 
       // setup debug draw
       var debugDraw = new box2d.b2DebugDraw();
-      debugDraw.SetSprite(stage.canvas.getContext('2d'));
-      debugDraw.SetDra
+      debugDraw.SetSprite(debug.getContext('2d'));
+      debugDraw.SetDrawScale(SCALE);
+      debugDraw.SetFlags(box2d.b2DebugDraw.e_shapeBit | box2d.b2DebugDraw.e_jointBit);
+      world.SetDebugDraw(debugDraw);
     }
 
     function tick() {
+      stage.update();
+      // uncomment to debug draw
+      world.DrawDebugData();
+      world.Step(1/60, 10, 10);
+      world.ClearForces();
+    }
 
+    function Ball() {
+      this.view = new createjs.Bitmap('../img/dude-test.png');
+      this.view.regX = 150;
+      this.view.regY = 150;
+      var fixDef = new box2d.b2FixtureDef();
+      fixDef.density = 1;
+      fixDef.friction = 0.5;
+      fixDef.restitution = 0.5;
+      var bodyDef = new box2d.b2BodyDef();
+      bodyDef.type = box2d.b2Body.b2_dynamicBody;
+      bodyDef.position.x = Math.random() * 1024 / SCALE;
+      bodyDef.position.y = 0;
+      fixDef.shape = new box2d.b2CircleShape(150 / SCALE);
+      this.view.body = world.CreateBody(bodyDef);
+      this.view.body.CreateFixture(fixDef);
+      this.view.on('tick', function (e) {
+        this.x = this.body.GetPosition().x * SCALE;
+        this.y = this.body.GetPosition().y * SCALE;
+        this.rotation = this.body.GetAngle() * (180/Math.PI);
+      });
     }
 
   }
